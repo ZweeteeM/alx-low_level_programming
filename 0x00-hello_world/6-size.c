2ac4e0cbde0f708d467a660e8fda6015ce03342d
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-02-16 14:09:47 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *Main - Entry point "prints the size of various types"
- *Return : Always 0 (success)
+ *Main - Entry point
+ *Return: Always 0 (success)
 */
 int main(void)
 {
