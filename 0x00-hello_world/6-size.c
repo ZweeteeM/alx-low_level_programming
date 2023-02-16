7c085c1b59877f668455ba033a416aca842bdf70
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-02-16 13:56:51 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry prints the size of various types
- * Return: Always 0 (Success)
+ *Main prints the size of various types
+ *return : Always 0 (success)
 */
-int main()
-{
-  printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
-  printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
-  printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
-  printf("Size of Double Data Types in C = %2d bytes \n", sizeof(double));
-  printf("Size of Long Double Data Types in C = %2d bytes \n", sizeof(long double));
-  printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
-  return 0;
+int main(void)
+{printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
