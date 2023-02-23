aa9d7aa95c14dc70d8061c20af4117f2e4473da2
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-02-23 14:11:34 -0800

    The numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_numbers - checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ *print_numbers - checks for a digit (0 through 9).
+ *Return: Always 0.
 */
 void print_numbers(void)
 {
 	int c;
-	
+
 	for (c = 48; c < 58; c++)
 	{
 		_putchar(c);
