52ea2e080edf7856f2892f774d97b8f5dd134b89
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-02-27 12:41:43 -0800

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 -function that prints every other character of a string
+ * starting with the first character, followed by a new line
+ * @str: string to print
+ * Return: 0
+*/
+void puts2(char *str)
+{
+	int x = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		x++;
+	}
+	t = x - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
