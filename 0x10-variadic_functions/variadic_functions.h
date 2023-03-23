8ee48ef378d3cae1849a9eb50fb99b00b25f2c6f
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-03-23 13:21:48 -0700

    Task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,14 @@
-#ifndef _variadic_functions_h
-#define _variadic_functions_h
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
-int_putchar(char);
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
