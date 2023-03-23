d868729eec3f77e90b58c545f0ad34622ad6c332
ZweeteeM <mugodozwiitwaho@gmail.com>
2023-03-23 13:47:23 -0700

    Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
-#ifndef function_pointers_h
-#define function_pointers_h
-
-#include <stdlib,h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdlib.h>
 #include <stddef.h>
 
 int _putchar(char c);
@@ -10,4 +9,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-
